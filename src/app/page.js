d3d211e81be081b51673139ac1df3ba12b82909b
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-04-29 22:59:52 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+
+
+export default function Home() {
+  return (
+    <h2 className="bg-red-500">Home Page</h2>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../public/images.png"
+import { Avatar, Button } from "@heroui/react";
+// import { authClient } from "@/lib/auth-client";
+
+const Navbar = () => {
+// const userData = authClient.useSession();
+// const user = userData.data?.user;
+// const handleSignOut = async () => {
+// await authClient.signOut();
+// }
+
+
+
+  return (
+    <div className=" border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={logo}
+            alt="logo"
+            loading="eager"  
+            width={30}
+            height={30}
+            className="object-cover h-auto w-auto"
+          />
+          <h3 className="font-black text-lg">pixgen.</h3>
+        </div>
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}> Home </Link>
+          </li>
+          <li>
+            <Link href={"/all-photos"}>All Photos</Link>
+          </li>
+          <li>
+            <Link href={"/pricing"}>Pricing</Link>
+          </li>
+          <li>
+            <Link href={"/profile"}>Profile</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          {/* {!user && 
+            <ul className="flex items-center gap-4  text-sm">
+            <li>
+              <Link href={"/signup"}><Button >SignUp</Button></Link>
+            </li>
+            <li>
+              <Link href={"/signin"}><Button>SignIn</Button></Link>
+            </li>
+          </ul>}
+
+          {user && (
+            <div className="flex gap-3">
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+
+              <Button onClick={handleSignOut} size="sm" variant="danger">SignOut</Button>
+            </div>
+          )} */}
+
+<ul className="flex items-center gap-4  text-sm">
+            <li>
+              <Link href={"/signup"}><Button>SignUp</Button></Link>
+            </li>
+            <li>
+              <Link href={"/signin"}><Button>SignIn</Button></Link>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
