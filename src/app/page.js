4f30c7f28c4c7cc3349d6e729b46b5fdfc201589
Sobuj <sobujmadbor660@gmail.com>
2026-09-26 00:13:05 +0600

    Refactor page structure: add new components and reorganize layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,16 @@
 import Banner from "@/components/Banner";
+import FAQ from "@/components/FAQ";
 import FeaturedTiles from "@/components/FeaturedTiles";
+import FinalCTA from "@/components/FinalCTA";
+import HowItWorks from "@/components/HowItWorks";
+import InteriorInspiration from "@/components/InteriorInspiration";
 import MarqueePage from "@/components/Marquar";
+import ModernCollection from "@/components/ModernCollection";
+import ShopByCategory from "@/components/ShopByCategory";
+import StatsSection from "@/components/StatsSection";
+import Testimonials from "@/components/Testimonials";
+import TrendingTiles from "@/components/TrendingTiles";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 
@@ -9,7 +19,17 @@ export default function Home() {
  <div>
     <Banner></Banner>
 <MarqueePage></MarqueePage>
+<ShopByCategory />
 <FeaturedTiles></FeaturedTiles>
+<WhyChooseUs />
+<ModernCollection />
+<HowItWorks />
+{/* <TrendingTiles />
+<InteriorInspiration /> */}
+{/* <Testimonials /> */}
+<StatsSection />
+<FAQ />
+<FinalCTA />
  </div>
   
   );
